9f8e96c9b21330a4e4a509e547a0563c72700e02
KaiqiGeorge <oliviakaiqihuang@gmail.com>
2022-07-05 13:16:30 -0700

    added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,9 @@
+import React from "react";
 import "./Weather.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from "axios";
+
+
 
 export default function Weather() {
   let weatherData = {
@@ -13,68 +18,83 @@ export default function Weather() {
   };
   return (
     <div className="wrapper">
-    <div className="container">
-      <form className="search-form">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city.."
-              className="form-control cityInput"
-            />
+      <div className="container">
+        <form className="search-form">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                className="form-control cityInput"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary search"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="search"
-              className="btn btn-primary search"
-            />
-          </div>
-        </div>
-      </form>
+        </form>
 
-      <h1 className="cityName">{weatherData.city}</h1>
-      <ul className="time-weather">
-        <li>
-          Last updated: <span id="time">{weatherData.date}</span>
-        </li>
-        <li className="weather-description">{weatherData.description}</li>
-      </ul>
-      <div className="row">
-        <div className="col-7">
-          <img
-            className="float-left icon"
-            src={weatherData.imgUrl}
-            alt={weatherData.description}
-          />
-          <span className="temperature-number">{weatherData.temperature}</span>
-          <span className="units">
-            <a className="active Celsius" href="/">
-              °C|
-            </a>
-            <a className="Fah" href="/">
-              °F
-            </a>
-          </span>
+        <h1 className="cityName">{weatherData.city}</h1>
+        <ul className="time-weather">
+          <li>
+            Last updated: <span id="time">{weatherData.date}</span>
+          </li>
+          <li className="weather-description">{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-7">
+            <img
+              className="float-left icon"
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+            />
+            <span className="temperature-number">
+              {weatherData.temperature}
+            </span>
+            <span className="units">
+              <a className="active Celsius" href="/">
+                °C|
+              </a>
+              <a className="Fah" href="/">
+                °F
+              </a>
+            </span>
+          </div>
+          <div className="col-5">
+            <ul className="weather-detail">
+              <li className="humidity">Humidity: {weatherData.humidity}%</li>
+              <li className="clouds">Clouds: {weatherData.clouds}%</li>
+              <li className="wind">Wind: {weatherData.wind}m/s</li>
+            </ul>
+          </div>
         </div>
-        <div className="col-5">
-          <ul className="weather-detail">
-            <li className="humidity">Humidity: {weatherData.humidity}%</li>
-            <li className="clouds">Clouds: {weatherData.clouds}%</li>
-            <li className="wind">Wind: {weatherData.wind}m/s</li>
-          </ul>
+        <div className="row weather-forecast">
+          <div className="col-2">
+            <div className="weather-forecast-day"></div>
+            <div className="weather-forecast-icon"></div>
+            <div class="weather-forecast-tempMin"></div>
+            <div class="weather-forecast-tempMax"></div>
+          </div>
         </div>
       </div>
-      <div className="row weather-forecast">
-        <div className="col-2">
-          <div className="weather-forecast-day"></div>
-          <div className="weather-forecast-icon"></div>
-          <div class="weather-forecast-tempMin"></div>
-          <div class="weather-forecast-tempMax"></div>
-        </div>
-      </div>
-    </div>
-    <footer> <a href="https://github.com/KaiqiGeorge/Weather-app-react">Open-source</a> coded by Kaiqi George</footer>
+      <footer>
+        {" "}
+        This project was coded by Kaiqi George and it is{" "}
+        <a
+          href="https://github.com/KaiqiGeorge/Weather-app-react"
+          target="_blank" rel="noreferrer" 
+        >
+          open-sourced
+        </a>{" "}
+        on Github and hosted on{" "}
+        <a href="https://kaiqi-weather-app-react.netlify.app/" target="_blank" rel="noreferrer" >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
